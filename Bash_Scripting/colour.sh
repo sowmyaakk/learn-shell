a4ac01bed27925150e6eb3ed606c9264a2b5f0a8
Sowmya <akkina.sowmya@capgemini.com>
2025-09-01 16:17:26 +0530

    adding varabiles

--- Bash_Scripting/colour.sh
+++ Bash_Scripting/colour.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+#printing values with different font colour
+
 echo -e "\e[31mThis is red text\e[0m"
 
 echo -e "Hi\t this\t is\t sowmya\t akkina"
 
+#printing values with different font colour and backgroung color
+
 echo -e "\e[34;43m sowmyaakkina\e[0m"
+
+#variables
 
 name=lokesh
 empid=1272
@@ -12,10 +18,16 @@ empid=1272
 echo -e "employee name is \e[31m $name \e[0m"
 echo -e "and employee id is \e[34m $empid \e[0m"
 
-#dynamicvarabiles
+#to get dynamic values from the system
+
+echo -e "the script is executed by $(whoami)"
+echo -e "today's date is $(date)"
+
+#to get dynamicvariables values from command line
 
 employeeName=$1
 employeeId=$2
 
 echo -e "employee name is \e[31m $employeeName \e[0m"
 echo -e "and employee id is \e[33m $employeeId \e[0m"
+
