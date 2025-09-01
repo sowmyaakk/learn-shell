a4dddb016664c3b3a4844849d43849c3d76af470
Sowmya <akkina.sowmya@capgemini.com>
2025-09-01 15:43:06 +0530

    adding a lines related to varabiles

--- Bash_Scripting/colour.sh
+++ Bash_Scripting/colour.sh
@@ -5,3 +5,9 @@ echo -e "\e[31mThis is red text\e[0m"
 echo -e "Hi\t this\t is\t sowmya\t akkina"
 
 echo -e "\e[34;43m sowmyaakkina\e[0m"
+
+name=lokesh
+empid=1272
+
+echo -e "employee name is \e[31m $name \e[0m"
+echo -e "and employee id is \e[32m $empid \e[0m"
