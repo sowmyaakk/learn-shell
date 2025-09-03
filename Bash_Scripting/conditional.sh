2b765b66203bcb558b2c7a1285c4fedfa69ec707
Sowmya <akkina.sowmya@capgemini.com>
2025-09-03 15:20:33 +0530

    adding read values

--- Bash_Scripting/conditional.sh
+++ Bash_Scripting/conditional.sh
@@ -3,9 +3,9 @@
 name=$1
 
 if [ $name = sowmya ]; then
-echo -e "\e[31m Hello Sowmya, Welcome to Bash Scripting \e[0m"
-exit 0
+    echo -e "\e[31m Hello Sowmya, Welcome to Bash Scripting \e[0m"
+    exit 0
 else
-echo -e "\e[32m Hello $name, you are not registered with this course \e[0m"
-exit 3
+    echo -e "\e[32m you are not registered with this course \e[0m"
+    exit 3
 fi
